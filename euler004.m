99aa74382bd3d354e0068c9405861ab86cb4a0c7
Andrew Nolan <35940923+acnolan@users.noreply.github.com>
2023-07-09 22:55:41 -0400

    Initial solution, not optimized yet

---
+++ euler004.m
@@ -0,0 +1,21 @@
+% A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
+% Find the largest palindrome made from the product of numbers less than or equal to the input number.
+function y = euler004(x)
+    % If no numbers are palindromes at least 0 probably is
+    y = 0;
+
+    % Two loops to multiply smaller numbers
+    % The largest one will be made of the largest numbers so we decrement
+    % Once we find a large number we can break.
+    for i = x:-1:1
+        for j = x:-1:1
+            test = i * j;
+            if string(test) == reverse(string(test))
+                if test > y
+                    y = test;
+                    break;
+                end
+            end
+        end
+    end
+end
